e0c34ab4cd32787e18c3aed4622a01ae3a588c93
JL <jlvsolmeron@addu.edu.ph>
2020-05-19 14:45:50 +0800

    Michael Azcueta final changes

--- src/keys/index.js
+++ src/keys/index.js
@@ -1,4 +1,4 @@
 module.exports = {
-    api_base_url: 'http://vps57141.inmotionhosting.com:5001',
-    // api_base_url: 'http://localhost:5001',
+    //api_base_url: 'http://vps57141.inmotionhosting.com:5001',
+    api_base_url: 'http://localhost:5001',
 }
